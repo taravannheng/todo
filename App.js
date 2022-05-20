638d2d6cdd18bc2342b1f4398c06b7a14025f5a9
taravannheng <heng.t.taravann@gmail.com>
2022-05-20 19:48:12 +0700

    feat: Add add new todo button

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World!</Text>
+      <Button title="Add New Todo" />
       <StatusBar style="auto" />
     </View>
   );
